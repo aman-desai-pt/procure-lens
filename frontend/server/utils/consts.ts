445372a9d791b95b8895d9b54d4ee1ba265f17aa
amandesai01 <amandesai01@gmail.com>
2025-01-23 07:21:24 +0530

    path fix

--- frontend/server/utils/consts.ts
+++ frontend/server/utils/consts.ts
@@ -1,4 +1,4 @@
-export const baseDir = import.meta.dev ? './.data' : '/var/lib/procure-lens'
+export const baseDir = './.data'
 
 export const BLOB_STORAGE_KEY = 'blobStorage';
 export const blobStorage = useStorage(BLOB_STORAGE_KEY);
